cf552a8005fbfe811068a2dbdb52669d664b87fb
Ebbez <7920708+ebbez@users.noreply.github.com>
2026-01-18 19:07:49 +0100

    git-pages for Forgejo

--- compose-files/forgejo/prep_runner_and_pages_dirs.sh
+++ compose-files/forgejo/prep_runner_and_pages_dirs.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-mkdir -p data/.cache
+mkdir -p runnerdata/.cache
 
 chown -R 1001:1001 runnerdata
 chmod 775 runnerdata/.cache
 chmod g+s runnerdata/.cache
+
+mkdir -p pages/data
+chown -R 1002:1002 pages/data
